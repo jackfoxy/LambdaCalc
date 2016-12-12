9e7de105295d5dd76e01d06b301c50bcf1515f55
Jack Fox <jackfoxy@gmail.com>
2016-12-12 08:41:59 -0800

    reorg ast and core

--- src/UntypedRecurs/Scripts/load-project-debug.fsx
+++ src/UntypedRecurs/Scripts/load-project-debug.fsx
@@ -1,9 +1,13 @@
 // Warning: generated file; your changes could be lost when a new file is generated.
 #I __SOURCE_DIRECTORY__
 #load "load-references-debug.fsx"
-#load "../Parser.fsi"
+#load "../Ast.fsi"
+      "../Ast.fs"
+      "../Parser.fsi"
       "../Parser.fs"
       "../Lexer.fs"
+      "../Core.fsi"
+      "../Core.fs"
       "../Reduce.fsi"
       "../Reduce.fs"
       "../Main.fs"
